08a8df25d47cd2196dbedaf93290b9be1bfe87bb
York <yorkzhang520@gmail.com>
2020-02-13 10:26:33 +0800

    change(Flr): 修改plugin id为 com.fly-mix.flr ； 更新changeNotes

--- src/main/java/com/flr/FlrConstant.java
+++ src/main/java/com/flr/FlrConstant.java
@@ -2,7 +2,7 @@ package com.flr;
 
 public class FlrConstant {
 
-    public static String flrId = "com.fly-mix.flr-as-plugin";
+    public static String flrId = "com.fly-mix.flr";
 
     public static String flrDisplayName = "Flr";
 
