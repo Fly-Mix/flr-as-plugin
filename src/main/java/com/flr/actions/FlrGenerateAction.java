be65a9b2f3434ea2a0c3faf531df489ca7f5d8b0
York <yorkzhang520@gmail.com>
2020-02-13 09:37:27 +0800

    refactor(FlrGenerateAction): 优化日志输出，若当前有资源变化监控服务在运行，则不清空日志，并在执行generate任务结束后，提醒有资源变化监控服务在运行

--- src/main/java/com/flr/actions/FlrGenerateAction.java
+++ src/main/java/com/flr/actions/FlrGenerateAction.java
@@ -20,13 +20,28 @@ public class FlrGenerateAction extends AnAction {
 
         FlrLogConsole flrLogConsole = FlrLogConsoleFactory.createLogConsole(project);
         FlrLogConsoleFactory.showCurLogConsole(project);
-        flrLogConsole.clear();
 
         FlrApp flrApp = project.getComponent(FlrApp.class);
+
+        // 如果当前资源变化监控服务正在运行，则不清空当前日志，否则就清空
+        if(flrApp.flrCommand.isMonitoringAssets == false) {
+            flrLogConsole.clear();
+        }
+
         ProgressManager.getInstance().run(new Task.Backgroundable(project, "Flr Generate", false) {
             @Override
             public void run(@NotNull ProgressIndicator indicator) {
                 flrApp.flrCommand.generate(e, flrLogConsole);
+
+                // 如果当前资源变化监控服务正在运行，则在执行 generate 后，打印监控服务在运行的提示
+                if(flrApp.flrCommand.isMonitoringAssets) {
+                    FlrLogConsole.LogType indicatorType = FlrLogConsole.LogType.normal;
+                    flrLogConsole.println("", indicatorType);
+                    String indicatorMessage =
+                            "[*]: the monitoring service is monitoring the asset changes, and then auto scan assets, specifies assets and generates \"r.g.dart\" ...\n" +
+                                    "[*]: you can click menu \"Tools-Flr-Stop Monitor\" to terminate it\n";
+                    flrLogConsole.println(indicatorMessage, indicatorType);
+                }
             }
         });
     }
