b5de6c376b83a58f37de463c7e0f0c36a7905861
York <yorkzhang520@gmail.com>
2021-03-28 21:51:10 +0800

    feat(FlrCommand): 新增 shouldSupportNullsafety 环境属性
    shouldSupportNullsafety 决定了生成的R代码是否支持 Nullsafety

--- src/main/java/com/flr/command/FlrCommand.java
+++ src/main/java/com/flr/command/FlrCommand.java
@@ -53,8 +53,31 @@ public class FlrCommand implements Disposable {
 
     private FlrLogConsole.LogType titleLogType = FlrLogConsole.LogType.tips;
 
+    private boolean shouldSupportNullsafety = false;
+
     public FlrCommand(Project project) {
         curProject = project;
+
+        initFlagOfShouldSupportNullsafety();
+    }
+
+    private void initFlagOfShouldSupportNullsafety() {
+        this.shouldSupportNullsafety = false;
+
+        VirtualFile flutterSdkHome = FlutterSdk.getFlutterSdk(curProject).getHome();
+        FlutterSdkVersion flutterSdkVersion = FlutterSdkVersion.readFromSdk(flutterSdkHome);
+        String flutterVersionWithoutHotfixStr = flutterSdkVersion.toString();
+
+        DartSdk dartSdk = DartSdk.getDartSdk(curProject);
+        String dartSdkVersionStr = (dartSdk == null) ? "0.0.0" : dartSdk.getVersion();
+
+        int compareResult = FlrUtil.versionCompare(flutterVersionWithoutHotfixStr, "1.10.15");
+        if(compareResult == 0 || compareResult == 1) {
+            compareResult = FlrUtil.versionCompare(dartSdkVersionStr, "2.12.0");
+            if(compareResult == 0 || compareResult == 1) {
+                this.shouldSupportNullsafety = true;
+            }
+        }
     }
 
     @Override
@@ -64,6 +87,9 @@ public class FlrCommand implements Disposable {
             curFlrListener = null;
         }
     }
+
+    // MARK: Private Util Methods
+
 
     // MARK: Command Action Methods
 
